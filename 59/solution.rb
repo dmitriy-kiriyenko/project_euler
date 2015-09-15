439c97bf0a10620720d82a4089041bb796ed14ef
Dmitriy Kiriyenko <(empty)>
2015-09-15 22:09:52 +0300

    Be smart.

--- 59/solution.rb
+++ 59/solution.rb
@@ -1,6 +1,17 @@
+PASS_SIZE = 3
+PASS_CHARS = ('a'.ord..'z'.ord)
+GOOD_CHARS = (' '.ord..'z'.ord)
+
 input = File.read(File.expand_path("input.txt", File.dirname(__FILE__))).chomp.split(",").map(&:to_i)
-keys = ('a'.ord..'z'.ord).to_a.combination(3).flat_map { |k| k.permutation.to_a }
-decrypts = keys.lazy.map { |key| input.zip(key.cycle).map { |(c, d)| c^d} }
-first, last = ' '.ord, 'z'.ord
-good_decrypts = decrypts.select { |decrypt| decrypt.all? { |c| first <= c && c <= last } }
-puts good_decrypts.detect { |decrypt| decrypt.map(&:chr).join.include?('the') }.reduce :+
+piece, *pieces = input.each_slice(PASS_SIZE).to_a
+
+pass_range = piece.zip(*pieces).map { |slice|
+  PASS_CHARS.select { |ch|
+    slice.compact.uniq.all? { |l| GOOD_CHARS.include? l^ch }
+  }
+}
+
+passwords = pass_range.reduce { |acc, i| acc.product(i).map(&:flatten) }.map
+texts = passwords.lazy.map { |pass| input.zip(pass.cycle).map { |(c, d)| c^d } }
+best = texts.detect { |decrypt| decrypt.map(&:chr).join.include?('the') }
+puts best.reduce :+

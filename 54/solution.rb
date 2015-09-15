b44c7e0bf725bccf2b0ec5b5d4617fbe29c2ac8e
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2015-09-15 12:41:07 +0300

    Local file.

--- 54/solution.rb
+++ 54/solution.rb
@@ -1,5 +1,3 @@
-require 'open-uri'
-
 VALUES = Hash['23456789TJQKA'.chars.each_with_index.map {|v, i| [v, i+2]}]
 STRAIGHTS = (5..14).step(1).map {|x| [x, x-1, x-2, x-3, x-4]} << [14, 5, 4, 3, 2]
 RANKS = [[1,1,1,1,1],[2,1,1,1],[2,2,1],[3,1,1],[],[],[3,2],[4,1]]
@@ -12,7 +10,7 @@ def rank(hand)
   [combo, values]
 end
 
-open("https://projecteuler.net/project/resources/p054_poker.txt") { |f|
+open(File.expand_path("input.txt", File.dirname(__FILE__))) { |f|
   puts f.each_line.reduce(0) { |acc, i|
     i = i.split(" ")
     acc + ((rank(i.take(5)) <=> rank(i.drop(5))) > 0 ? 1 : 0)

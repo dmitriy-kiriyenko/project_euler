509496e50d4bc52ed84087b6d7b3d3196e0fca80
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2015-09-16 16:16:07 +0300

    Just bugging.

--- 109/solution.rb
+++ 109/solution.rb
@@ -1,15 +1,14 @@
 require 'benchmark'
 
-THRESHOLD = 100
+THRESHOLD = 170
+ds = ((2..40).step(2).to_a << 50)
+THROWS = ((0..20).to_a + ds + (3..60).step(3).to_a << 25).sort.each_with_index.to_a
+DOUBLES = ds.sort.each_with_index.to_a
 
 def result
-  ds = ((2..40).step(2).to_a << 50)
-  throws = ((0..20).to_a + ds + (3..60).step(3).to_a << 25).sort.each_with_index.to_a
-  doubles = ds.sort.each_with_index.to_a
-
-  doubles.reduce(0) { |acc1, (a, i)|
-    acc1 + throws.take_while { |(b, _)| a + b < THRESHOLD }.reduce(0) { |acc2, (b, j)|
-      acc2 + (throws[j..-1].index { |(c, _)| a + b + c >= THRESHOLD } || (throws.length - j))
+  DOUBLES.reduce(0) { |acc1, (a, i)|
+    acc1 + THROWS.take_while { |(b, _)| a + b < THRESHOLD }.reduce(0) { |acc2, (b, j)|
+      acc2 + (THROWS[j..-1].index { |(c, _)| a + b + c >= THRESHOLD } || (THROWS.length - j))
     }.to_i
   }
 end

abd5caffba7685d0d9a3b83b271bca332b6289f3
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2015-09-16 16:12:02 +0300

    Bench all.

--- 54/solution.rb
+++ 54/solution.rb
@@ -1,18 +1,26 @@
+require 'benchmark'
+
 VALUES = Hash['23456789TJQKA'.chars.each_with_index.map {|v, i| [v, i+2]}]
 STRAIGHTS = (5..14).step(1).map {|x| [x, x-1, x-2, x-3, x-4]} << [14, 5, 4, 3, 2]
 RANKS = [[1,1,1,1,1],[2,1,1,1],[2,2,1],[3,1,1],[],[],[3,2],[4,1]]
 
-def rank(hand)
-  set, values = hand.group_by { |x| x[0]}.map {|(v, l)| [l.size, VALUES[v]]}.sort.reverse.transpose
-  combo = RANKS.index(set)
-  combo = 5 if hand.uniq {|x| x[1]}.size == 1
-  combo = (combo == 5 ? 8 : 4) if STRAIGHTS.include?(values)
-  [combo, values]
+def result
+  def rank(hand)
+    set, values = hand.group_by { |x| x[0]}.map {|(v, l)| [l.size, VALUES[v]]}.sort.reverse.transpose
+    combo = RANKS.index(set)
+    combo = 5 if hand.uniq {|x| x[1]}.size == 1
+    combo = (combo == 5 ? 8 : 4) if STRAIGHTS.include?(values)
+    [combo, values]
+  end
+
+  open(File.expand_path("input.txt", File.dirname(__FILE__))) { |f|
+    f.each_line.reduce(0) { |acc, i|
+      i = i.split(" ")
+      acc + ((rank(i.take(5)) <=> rank(i.drop(5))) > 0 ? 1 : 0)
+    }
+  }
 end
 
-open(File.expand_path("input.txt", File.dirname(__FILE__))) { |f|
-  puts f.each_line.reduce(0) { |acc, i|
-    i = i.split(" ")
-    acc + ((rank(i.take(5)) <=> rank(i.drop(5))) > 0 ? 1 : 0)
-  }
+puts Benchmark.measure {
+  puts result
 }
--- 59/solution.rb
+++ 59/solution.rb
@@ -1,17 +1,25 @@
+require 'benchmark'
+
 PASS_SIZE = 3
 PASS_CHARS = ('a'.ord..'z'.ord)
 GOOD_CHARS = (' '.ord..'z'.ord)
 
-input = File.read(File.expand_path("input.txt", File.dirname(__FILE__))).chomp.split(",").map(&:to_i)
-piece, *pieces = input.each_slice(PASS_SIZE).to_a
+def result
+  input = File.read(File.expand_path("input.txt", File.dirname(__FILE__))).chomp.split(",").map(&:to_i)
+  piece, *pieces = input.each_slice(PASS_SIZE).to_a
 
-pass_range = piece.zip(*pieces).map { |slice|
-  PASS_CHARS.select { |ch|
-    slice.compact.uniq.all? { |l| GOOD_CHARS.include? l^ch }
+  pass_range = piece.zip(*pieces).map { |slice|
+    PASS_CHARS.select { |ch|
+      slice.compact.uniq.all? { |l| GOOD_CHARS.include? l^ch }
+    }
   }
-}
 
-passwords = pass_range.reduce { |acc, i| acc.product(i).map(&:flatten) }.map
-texts = passwords.lazy.map { |pass| input.zip(pass.cycle).map { |(c, d)| c^d } }
-best = texts.detect { |text| text.map(&:chr).join.include?('the') }
-puts best.reduce :+
+  passwords = pass_range.reduce { |acc, i| acc.product(i).map(&:flatten) }.map
+  texts = passwords.lazy.map { |pass| input.zip(pass.cycle).map { |(c, d)| c^d } }
+  best = texts.detect { |text| text.map(&:chr).join.include?('the') }
+  best.reduce :+
+end
+
+puts Benchmark.measure {
+  puts result
+}
